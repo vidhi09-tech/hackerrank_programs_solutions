44a13ad01901b9c33d8d28551b5aa0c40e384243
VIDHI KUMARI <63573906+vidhi09-tech@users.noreply.github.com>
2020-09-15 20:43:30 +0530

    Add files via upload

---
+++ breaking_the_record/breaking_the_record.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the breakingRecords function below.
+def breakingRecords(scores):
+    mincount=maxcount=0
+    minscore=maxscore=scores[0]
+    for i in range (1, len(scores)):
+        if minscore<scores[i]:
+            minscore=scores[i]
+            mincount+=1
+        elif maxscore>scores[i]:
+            maxscore=scores[i]
+            maxcount+=1
+    return mincount, maxcount
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
